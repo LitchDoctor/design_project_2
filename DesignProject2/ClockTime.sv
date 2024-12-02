e64ce732e6f29192c799f42beae84bcf845a26c2
Ethan Marreel <ethan@marreel.com>
2024-12-02 15:09:53 -0800

    Update ClockTime.sv

--- DesignProject2/ClockTime.sv
+++ DesignProject2/ClockTime.sv
@@ -18,39 +18,26 @@ module ClockTime(
                 minutes <= 0;
                 hours <= 0;
             end else begin
-					/*case(ORDER)
-						2'b00: begin
-							seconds <= seconds + 1; 
-						end
-						2'b01: begin
-							minutes <= minutes + 1;
-						end
-						2'b10: begin
-							hours <= hours + 1;
-						end
-						
-					endcase*/
-				
-
 				   counter <= counter + 1;
 				   if (counter >= CLOCK_FREQ - 1) begin
-						  counter <= 0;
-						  seconds <= seconds + 1; 
+						counter <= 0;
+						seconds <= seconds + 1; 
+
+						//Loop Checks
+						if (seconds >= (60-1)) begin
+							seconds <= 0;
+							minutes <= minutes+1;
+						end//endif
+						
+						if (minutes >= (60-1)) begin
+							minutes <= 0;
+							hours <= hours + 1;
+						end //endif
+						
+						if (hours >= (24-1)) hours <= 0;
 					end//endif
             
-					//Loop Checks
-					if (seconds >= 59) begin
-						seconds <= 0;
-						minutes <= minutes+1;
-					end//endif
-					
-					if (minutes >= 59) begin
-						minutes <= 0;
-						hours <= hours + 1;
-					end //endif
-					
-					
-					if (hours >= 23) hours <= 0;
+
 			end //end - if not rst
     end //end always block
 endmodule
